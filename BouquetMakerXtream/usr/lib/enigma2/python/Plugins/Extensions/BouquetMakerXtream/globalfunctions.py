57cca683fe7b12324974d6a148d7565caf650855
kiddac <kiddac2015@gmail.com>
2023-09-03 12:47:24 +0100

    moved download urls into global

--- BouquetMakerXtream/usr/lib/enigma2/python/Plugins/Extensions/BouquetMakerXtream/globalfunctions.py
+++ BouquetMakerXtream/usr/lib/enigma2/python/Plugins/Extensions/BouquetMakerXtream/globalfunctions.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from .plugin import playlists_json
+from .plugin import playlists_json, hdr
 
 from enigma import eDVBDB
+from requests.adapters import HTTPAdapter
 
 import json
 import os
 import re
+import requests
 
 
 def getPlaylistJson():
@@ -24,6 +26,81 @@ def getPlaylistJson():
 def refreshBouquets():
     eDVBDB.getInstance().reloadServicelist()
     eDVBDB.getInstance().reloadBouquets()
+
+
+def download_url(url, ext):
+    print("*** url ***", url)
+    r = ""
+    retries = 0
+    adapter = HTTPAdapter(max_retries=retries)
+    http = requests.Session()
+    http.mount("http://", adapter)
+    http.mount("https://", adapter)
+    r = ""
+    try:
+        r = http.get(url, headers=hdr, timeout=(10, 30), verify=False)
+        r.raise_for_status()
+        if r.status_code == requests.codes.ok:
+            try:
+                if ext == "json":
+                    response = r.json()
+                else:
+                    response = r.content
+                return response
+            except Exception as e:
+                print(e)
+                return ""
+
+    except Exception as e:
+        print(e)
+
+    return ""
+
+
+def download_url_multi(url):
+
+    category = url[1]
+    ext = url[2]
+    # print("**** url ***", url)
+    r = ""
+    retries = 1
+    adapter = HTTPAdapter(max_retries=retries)
+    http = requests.Session()
+    http.mount("http://", adapter)
+    http.mount("https://", adapter)
+    response = ""
+    try:
+        r = http.get(url[0], headers=hdr, timeout=(10, 30), verify=False)
+        r.raise_for_status()
+        if r.status_code == requests.codes.ok:
+
+            try:
+                if ext == "json":
+                    response = category, r.json()
+                else:
+                    response = category, r.text
+                return response
+            except Exception as e:
+                print(e)
+                return category, ""
+
+    except Exception as e:
+        print(e)
+
+    return category, ""
+
+
+def safeName(name):
+    for char in name:
+        if not char.isalnum():
+            name = name.strip()
+            name = name.replace(char, "_")
+
+    name = re.sub(r"_+", "_", name)
+    name = re.sub(r"__", "_", name)
+    name = name.strip("_")
+
+    return name
 
 
 def purge(dir, pattern):
