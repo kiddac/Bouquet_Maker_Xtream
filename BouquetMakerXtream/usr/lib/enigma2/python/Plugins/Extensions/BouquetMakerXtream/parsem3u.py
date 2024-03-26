9fde7121eea294d1bcb45fadd39d76a21473dabf
kiddac <kiddac2015@gmail.com>
2024-03-26 19:31:29 +0000

    refactored all the code. Removed some bouquet header markers. Better handling of python split() to prevent crashes

--- BouquetMakerXtream/usr/lib/enigma2/python/Plugins/Extensions/BouquetMakerXtream/parsem3u.py
+++ BouquetMakerXtream/usr/lib/enigma2/python/Plugins/Extensions/BouquetMakerXtream/parsem3u.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import os
+import re
+
 from . import _
 from . import bouquet_globals as glob
 from .plugin import cfg, pythonVer
-
-import os
-import re
 
 
 def parseM3u8Playlist(response):
@@ -17,22 +17,21 @@ def parseM3u8Playlist(response):
     name = ""
     streamid = 0
 
-    if glob.current_playlist["playlist_info"]["playlist_type"] == "local":
-        local_file = os.path.join(cfg.local_location.value + glob.current_playlist["playlist_info"]["full_url"])
-        with open(local_file) as f:
+    playlist_type = glob.current_playlist["playlist_info"]["playlist_type"]
+
+    if playlist_type == "local":
+        local_file = os.path.join(cfg.local_location.value, glob.current_playlist["playlist_info"]["full_url"])
+        with open(local_file, encoding="utf-8") as f:
             response = f.readlines()
 
-    elif glob.current_playlist["playlist_info"]["playlist_type"] == "external":
+    elif playlist_type == "external":
         response = response.splitlines()
 
     for line in response:
-        if pythonVer == 3:
-            try:
-                line = line.decode("utf-8")
-            except:
-                pass
+        if pythonVer == 3 and isinstance(line, bytes):
+            line = line.decode("utf-8")
 
-        if not line.startswith("#EXTINF") and not line.startswith("http") and not line.startswith("#EXTGRP"):
+        if not (line.startswith("#EXTINF") or line.startswith("http") or line.startswith("#EXTGRP")):
             continue
 
         if line.startswith("#EXTINF"):
@@ -42,55 +41,49 @@ def parseM3u8Playlist(response):
             epg_id = ""
 
             # search logo first so we can delete it if base64 png
-            if "tvg-logo=" in line:
-                logo = re.search('tvg-logo="(.*?)"', line).group(1).strip()
-                if "data:image" in logo:
+            logo_match = re.search('tvg-logo="(.*?)"', line)
+            if logo_match:
+                logo = logo_match.group(1).strip()
+                if logo.startswith("data:image"):
                     logo = ""
                     line = re.sub('tvg-logo="(.*?)"', "", line)
 
-            if "group-title=" in line and "format" not in line:
-                try:
-                    group_title = re.search('group-title="(.*?)"', line).group(1).strip()
-                except:
-                    group_title = ""
+            group_title_match = re.search('group-title="(.*?)"', line)
+            group_title = group_title_match.group(1).strip() if group_title_match and "format" not in line else ""
 
-            if "tvg-name=" in line:
-                try:
-                    name = re.search('tvg-name="(.*?)"', line).group(1).strip()
-                except:
-                    name = line.strip().split(",")[1]
+            if ',' in line:
+                name_match = re.search('tvg-name="(.*?)"', line)
+                name = name_match.group(1).strip() if name_match else line.strip().split(",")[1]
 
-            else:
-                name = line.strip().split(",")[1]
-
-            if name == "":
+            if not name:
                 channel_num += 1
                 name = _("Stream") + " " + str(channel_num)
 
-            if "tvg-id=" in line:
-                try:
-                    epg_id = re.search('tvg-id=\"(.*?)\"', line).group(1).strip()
-                except:
-                    epg_id = ""
+            epg_id_match = re.search('tvg-id="(.*?)"', line)
+            epg_id = epg_id_match.group(1).strip() if epg_id_match else ""
 
-        elif line.startswith("#EXTGRP") and group_title == "":
-            try:
-                group_title = line.split(" ")[-1]
-            except:
-                group_title = ""
+        elif line.startswith("#EXTGRP") and not group_title:
+            group_title = line.split()[-1]
 
         elif line.startswith("http"):
             source = line.strip()
             stream_type = ""
 
-            if "/movie/" in source:
+            if "---" in name or "***" in name:
+                continue
+
+            if "/series/" in source:
+                stream_type = "series"
+
+            elif (source.endswith(".mp4") or source.endswith(".mkv") or source.endswith("avi")) and ("S0" in name or "E0" in name):
+                stream_type = "series"
+
+            elif "/movie/" in source:
                 stream_type = "vod"
-            elif "/series/" in source:
-                stream_type = "series"
-            elif "S0" in name or "E0" in name:
-                stream_type = "series"
+
             elif source.endswith(".mp4") or source.endswith(".mkv") or source.endswith("avi"):
                 stream_type = "vod"
+
             elif (
                 source.endswith(".ts")
                 or source.endswith(".m3u8")
@@ -106,89 +99,73 @@ def parseM3u8Playlist(response):
                 or (source[-1].isdigit())
             ):
                 stream_type = "live"
-            else:
-                continue
 
-            if stream_type == "live" and glob.current_playlist["settings"]["show_live"] is True:
-                if group_title == "":
-                    group_title = "Uncategorised Live"
-                streamid += 1
-                live_streams.append([epg_id, logo, group_title, name, source, streamid])
+            if name and source:
+                if stream_type == "live":
+                    group_title = group_title if group_title else "Uncategorised Live"
+                    streamid += 1
+                    live_streams.append([epg_id, logo, group_title, name, source, streamid])
 
-            elif stream_type == "vod" and glob.current_playlist["settings"]["show_vod"] is True:
-                if group_title == "":
-                    group_title = "Uncategorised VOD"
+                elif stream_type == "vod":
+                    group_title = group_title if group_title else "Uncategorised VOD"
+                    streamid += 1
+                    vod_streams.append([epg_id, logo, group_title, name, source, streamid])
 
-                streamid += 1
-                vod_streams.append([epg_id, logo, group_title, name, source, streamid])
+                elif stream_type == "series":
+                    group_title = group_title if group_title else "Uncategorised Series"
+                    streamid += 1
+                    series_streams.append([epg_id, logo, group_title, name, source, streamid])
 
-            elif stream_type == "series" and glob.current_playlist["settings"]["show_series"] is True:
-                if group_title == "":
-                    group_title = "Uncategorised Series"
-                streamid += 1
-                series_streams.append([epg_id, logo, group_title, name, source, streamid])
-
-            else:
-                if group_title == "":
-                    group_title = "Uncategorised"
-                streamid += 1
-                live_streams.append([epg_id, logo, group_title, name, source, streamid])
+                else:
+                    group_title = group_title if group_title else "Uncategorised"
+                    streamid += 1
+                    live_streams.append([epg_id, logo, group_title, name, source, streamid])
 
     return live_streams, vod_streams, series_streams
 
 
 def makeM3u8CategoriesJson(live_streams, vod_streams, series_streams):
-    glob.current_playlist["data"]["live_categories"] = []
-    glob.current_playlist["data"]["vod_categories"] = []
-    glob.current_playlist["data"]["series_categories"] = []
+    data = glob.current_playlist["data"]
+    data["live_categories"] = []
+    data["vod_categories"] = []
+    data["series_categories"] = []
 
     for x in live_streams:
-        if not glob.current_playlist["data"]["live_categories"]:
-            glob.current_playlist["data"]["live_categories"].append({"category_id": str(x[2]), "category_name": str(x[2])})
+        if not data["live_categories"]:
+            data["live_categories"].append({"category_id": str(x[2]), "category_name": str(x[2])})
         else:
-            exists = False
-            for category in glob.current_playlist["data"]["live_categories"]:
-                if category["category_name"] == str(x[2]):
-                    exists = True
-                    break
+            exists = any(category["category_name"] == str(x[2]) for category in data["live_categories"])
             if not exists:
-                glob.current_playlist["data"]["live_categories"].append({"category_id": str(x[2]), "category_name": str(x[2])})
+                data["live_categories"].append({"category_id": str(x[2]), "category_name": str(x[2])})
 
     for x in vod_streams:
-        if not glob.current_playlist["data"]["vod_categories"]:
-            glob.current_playlist["data"]["vod_categories"].append({"category_id": str(x[2]), "category_name": str(x[2])})
+        if not data["vod_categories"]:
+            data["vod_categories"].append({"category_id": str(x[2]), "category_name": str(x[2])})
         else:
-            exists = False
-            for category in glob.current_playlist["data"]["vod_categories"]:
-                if category["category_name"] == str(x[2]):
-                    exists = True
-                    break
+            exists = any(category["category_name"] == str(x[2]) for category in data["vod_categories"])
             if not exists:
-                glob.current_playlist["data"]["vod_categories"].append({"category_id": str(x[2]), "category_name": str(x[2])})
+                data["vod_categories"].append({"category_id": str(x[2]), "category_name": str(x[2])})
 
     for x in series_streams:
-        if not glob.current_playlist["data"]["series_categories"]:
-            glob.current_playlist["data"]["series_categories"].append({"category_id": str(x[2]), "category_name": str(x[2])})
+        if not data["series_categories"]:
+            data["series_categories"].append({"category_id": str(x[2]), "category_name": str(x[2])})
         else:
-            exists = False
-            for category in glob.current_playlist["data"]["series_categories"]:
-                if category["category_name"] == str(x[2]):
-                    exists = True
-                    break
+            exists = any(category["category_name"] == str(x[2]) for category in data["series_categories"])
             if not exists:
-                glob.current_playlist["data"]["series_categories"].append({"category_id": str(x[2]), "category_name": str(x[2])})
+                data["series_categories"].append({"category_id": str(x[2]), "category_name": str(x[2])})
 
 
 def makeM3u8StreamsJson(live_streams, vod_streams, series_streams):
-    glob.current_playlist["data"]["live_streams"] = []
-    glob.current_playlist["data"]["vod_streams"] = []
-    glob.current_playlist["data"]["series_streams"] = []
+    data = glob.current_playlist["data"]
+    data["live_streams"] = []
+    data["vod_streams"] = []
+    data["series_streams"] = []
 
     for x in live_streams:
-        glob.current_playlist["data"]["live_streams"].append({"epg_channel_id": str(x[0]), "stream_icon": str(x[1]), "category_id": str(x[2]), "name": str(x[3]), "source": str(x[4]), "stream_id": str(x[5]), "added": 0})
+        data["live_streams"].append({"epg_channel_id": str(x[0]), "stream_icon": str(x[1]), "category_id": str(x[2]), "name": str(x[3]), "source": str(x[4]), "stream_id": str(x[5]), "added": 0})
 
     for x in vod_streams:
-        glob.current_playlist["data"]["vod_streams"].append({"stream_icon": str(x[1]), "category_id": str(x[2]), "name": str(x[3]), "source": str(x[4]), "stream_id": str(x[5]), "added": 0})
+        data["vod_streams"].append({"stream_icon": str(x[1]), "category_id": str(x[2]), "name": str(x[3]), "source": str(x[4]), "stream_id": str(x[5]), "added": 0})
 
     for x in series_streams:
-        glob.current_playlist["data"]["series_streams"].append({"stream_icon": str(x[1]), "category_id": str(x[2]), "name": str(x[3]), "source": str(x[4]), "series_id": str(x[5]), "added": 0})
+        data["series_streams"].append({"stream_icon": str(x[1]), "category_id": str(x[2]), "name": str(x[3]), "source": str(x[4]), "series_id": str(x[5]), "added": 0})
